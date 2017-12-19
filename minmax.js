493629f7e9cdd76f87067207c64410a4879a5fe9
Kaelan Mikowicz <eternity01033@gmail.com>
2017-12-18 16:49:26 -0800

    fix tictactoe

--- minmax.js
+++ minmax.js
@@ -3,79 +3,81 @@ var globalBoardState = [
     [0, 0, 0],
     [0, 0, 0]
 ];
-function minimax(player, depth, boardState) {
-    function boardHeuristic(board) {
-        var sum = 0;
-        for(var i = 0; i < 3; i++) {
-            if(board[i][0] === board[i][1] && board[i][1] === board[i][2] && board[i][0] != 0) {
-                board[i][0] === player ? sum += 10000 : sum -= 10000;
-            }
-            else {
-                if(board[i][0] === board[i][1] && board[i][0] != 0 && board[i][2] == 0) {
-                    board[i][0] === player ? sum += 100 : sum -= 100;
-                }
-                if(board[i][1] === board[i][2] && board[i][1] != 0 && board[i][0] == 0) {
-                    board[i][1] === player ? sum += 100 : sum -= 100;
-                }
-                if(board[i][0] === board[i][2] && board[i][0] != 0 && board[i][1] == 0) {
-                    board[i][0] === player ? sum += 100 : sum -= 100;
-                }
-            }
-            if(board[0][i] === board[1][i] && board[1][i] === board[2][i] && board[0][i] != 0) {
-                board[0][i] === player ? sum += 10000 : sum -= 10000;
-            }
-            else {
-                if(board[0][i] === board[1][i] && board[0][i] != 0 && board[2][i] == 0) {
-                    board[0][i] === player ? sum += 100 : sum -= 100;
-                }
-                if(board[1][i] === board[2][i] && board[1][i] != 0 && board[0][i] == 0) {
-                    board[1][i] === player ? sum += 100 : sum -= 100;
-                }
-                if(board[0][i] === board[2][i] && board[0][i] != 0 && board[1][i] == 0) {
-                    board[0][i] === player ? sum += 100 : sum -= 100;
-                }
-            }
-        }
-        if(board[0][0] === board[1][1] && board[1][1] === board[2][2] && board[1][1] != 0) {
-            board[0][0] === player ? sum += 10000 : sum -= 10000;
+function boardHeuristic(player, board) {
+    var sum = 0;
+    for(var i = 0; i < 3; i++) {
+        if(board[i][0] === board[i][1] && board[i][1] === board[i][2] && board[i][0] != 0) {
+            board[i][0] === 1 ? sum += 10000 : sum -= 10000;
         }
         else {
-            if(board[0][0] === board[1][1] && board[1][1] != 0 && board[2][2] == 0) {
-                board[0][0] === player ? sum += 100 : sum -= 100;
+            if(board[i][0] === board[i][1] && board[i][0] != 0 && board[i][2] == 0) {
+                board[i][0] === 1 ? sum += 100 : sum -= 100;
             }
-            if(board[1][1] === board[2][2] && board[1][1] != 0 && board[0][0] == 0) {
-                board[1][1] === player ? sum += 100 : sum -= 100;
+            if(board[i][1] === board[i][2] && board[i][1] != 0 && board[i][0] == 0) {
+                board[i][1] === 1 ? sum += 100 : sum -= 100;
             }
-            if(board[0][0] === board[2][2] && board[0][0] != 0 && board[1][1] == 0) {
-                board[0][0] === player ? sum += 100 : sum -= 100;
+            if(board[i][0] === board[i][2] && board[i][0] != 0 && board[i][1] == 0) {
+                board[i][0] === 1 ? sum += 100 : sum -= 100;
             }
         }
-        if(board[2][0] === board[1][1] && board[1][1]=== board[0][2] && board[2][0] != 0) {
-            board[2][0] === player ? sum += 10000 : sum -= 10000;
+        if(board[0][i] === board[1][i] && board[1][i] === board[2][i] && board[0][i] != 0) {
+            board[0][i] === 1 ? sum += 10000 : sum -= 10000;
         }
         else {
-            if(board[2][0] === board[1][1] && board[1][1] != 0 && board[0][2] == 0) {
-                board[2][0] === player ? sum += 100 : sum -= 100;
+            if(board[0][i] === board[1][i] && board[0][i] != 0 && board[2][i] == 0) {
+                board[0][i] === 1 ? sum += 100 : sum -= 100;
             }
-            if(board[1][1] === board[0][2] && board[1][1] != 0 && board[2][0] == 0) {
-                board[1][1] === player ? sum += 100 : sum -= 100;
+            if(board[1][i] === board[2][i] && board[1][i] != 0 && board[0][i] == 0) {
+                board[1][i] === 1 ? sum += 100 : sum -= 100;
             }
-            if(board[2][0] === board[0][2] && board[0][2] != 0 && board[1][1] == 0) {
-                board[2][0] === player ? sum += 100 : sum -= 100;
+            if(board[0][i] === board[2][i] && board[0][i] != 0 && board[1][i] == 0) {
+                board[0][i] === 1 ? sum += 100 : sum -= 100;
             }
         }
-        return sum;
     }
-    var value = boardHeuristic(boardState);
-    let gameEnd = true;
+    if(board[0][0] === board[1][1] && board[1][1] === board[2][2] && board[1][1] != 0) {
+        board[0][0] === 1 ? sum += 10000 : sum -= 10000;
+    }
+    else {
+        if(board[0][0] === board[1][1] && board[1][1] != 0 && board[2][2] == 0) {
+            board[0][0] === 1 ? sum += 100 : sum -= 100;
+        }
+        if(board[1][1] === board[2][2] && board[1][1] != 0 && board[0][0] == 0) {
+            board[1][1] === 1 ? sum += 100 : sum -= 100;
+        }
+        if(board[0][0] === board[2][2] && board[0][0] != 0 && board[1][1] == 0) {
+            board[0][0] === 1 ? sum += 100 : sum -= 100;
+        }
+    }
+    if(board[2][0] === board[1][1] && board[1][1]=== board[0][2] && board[2][0] != 0) {
+        board[2][0] === 1 ? sum += 10000 : sum -= 10000;
+    }
+    else {
+        if(board[2][0] === board[1][1] && board[1][1] != 0 && board[0][2] == 0) {
+            board[2][0] === 1 ? sum += 100 : sum -= 100;
+        }
+        if(board[1][1] === board[0][2] && board[1][1] != 0 && board[2][0] == 0) {
+            board[1][1] === 1 ? sum += 100 : sum -= 100;
+        }
+        if(board[2][0] === board[0][2] && board[0][2] != 0 && board[1][1] == 0) {
+            board[2][0] === 1 ? sum += 100 : sum -= 100;
+        }
+    }
+    return sum;
+}
+function isFull(boardState) {
     for(var i = 0; i < 3; i++) {
         for(var j = 0; j < 3; j++) {
             if(boardState[i][j] == 0)
-                gameEnd = false;
+                return false;
         }
     }
-    if(depth == 0 || value >= 1000 || value <= -1000 || gameEnd)
-        return {val: value};
+    return true;
+}
+function minimax(player, depth, boardState) {
+    var value = boardHeuristic(player, boardState);
+    if(depth == 0 || value >= 1000 || value <= -1000 || isFull(boardState))
+        return {val: value, board: boardState};
     if(player == 1) {
         let bestVal = {val: -100000};
         for(var i = 0; i < 3; i++) {
@@ -92,7 +94,7 @@ function minimax(player, depth, boardState) {
                     b[i][j] = player;
                     let val = minimax(-1, depth - 1, b);
                     if(val.val > bestVal.val)
-                        bestVal = {val: val.val, x: j, y: i};
+                        bestVal = {val: val.val, x: j, y: i, board: val.board};
                 }
             }
         }
@@ -114,7 +116,7 @@ function minimax(player, depth, boardState) {
                     b[i][j] = player;
                     let val = minimax(1, depth - 1, b);
                     if(val.val < bestVal.val)
-                        bestVal = {val: val.val, x: j, y: i};
+                        bestVal = {val: val.val, x: j, y: i, board: val.board};
                 }
             }
         }
